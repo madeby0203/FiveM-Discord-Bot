261884f0e98e2ef3bb86e8799cd6ec6871aaa5ea
madeby0203 <42004135+madeby0203@users.noreply.github.com>
2019-03-07 15:00:49 +0100

    Added command logs

--- cogs/logs.py
+++ cogs/logs.py
@@ -7,6 +7,30 @@ class logs:
     def __init__(self, bot):
         self.bot = bot
 
+    async def on_command(self, ctx):
+        if ctx.message.guild == self.bot.get_guild(325842626601549827):
+            embed=discord.Embed(title=" ", color=0xffff00)
+            embed.set_author(name="Executed command")
+            embed.add_field(name="Command:", value=ctx.command, inline=True)
+            embed.add_field(name="Arguments:", value=ctx.kwargs, inline=False)
+            embed.add_field(name="User:", value=ctx.author, inline=True)
+            embed.add_field(name="Channel:", value=ctx.channel, inline=True)
+            embed.add_field(name="Cog:", value=ctx.cog, inline=True)
+            channel = self.bot.get_channel(552174763255136256)
+            await channel.send(embed = embed)
+
+    #async def on_command_error(self, ctx, error):
+    #    if ctx.message.guild == self.bot.get_guild(325842626601549827):
+    #        embed=discord.Embed(title=" ", color=0xff0000)
+    #        embed.set_author(name="Command Error")
+    #        embed.add_field(name="Command:", value=ctx.command, inline=True)
+    #        embed.add_field(name="Arguments:", value=ctx.kwargs, inline=False)
+    #        embed.add_field(name="User:", value=ctx.author, inline=True)
+    #        embed.add_field(name="Channel:", value=ctx.channel, inline=True)
+    #        embed.add_field(name="Error:", value=error, inline=True)
+    #        channel = self.bot.get_channel(552174763255136256)
+    #        await channel.send("<@153259480715362304>", embed = embed)
+
     async def on_message_delete(self, message):
         if message.guild == self.bot.get_guild(325842626601549827):
             msg = message.content
@@ -19,7 +43,7 @@ class logs:
             embed.add_field(name = "Channel:", value = channel, inline = True)
             channel = self.bot.get_channel(511217531852292096)
             await channel.send(embed = embed)
-            print(message.guild.audit_logs(limit=10, before=None, after=None, reverse=None, user=message.guild.me, action=None))
+            print(message.guild.audit_logs(limit=10, before=None, after=None, reverse=None, user=None, action=None))
 
     async def on_message_edit(self, before, after):
         if before.content == "":
