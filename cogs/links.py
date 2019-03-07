3fb5dc9b914b55aaac74eba96d0235935c7afafc
madeby0203 <42004135+madeby0203@users.noreply.github.com>
2019-03-07 15:01:11 +0100

    Links in embeds
    Links are provided in an embed

--- cogs/links.py
+++ cogs/links.py
@@ -14,18 +14,24 @@ class links:
         await ctx.message.delete()
 
     @commands.command(pass_context = True)
-    async def forum(self, ctx):
-        await ctx.send("<https://bigcityrp.com/forums/index.php>")
+    async def forums(self, ctx):
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Forums", value="https://bigcityrp.com/forums/index.php", inline=False)
+        await ctx.send(embed=embed)
         await ctx.message.delete()
 
     @commands.command(pass_context = True)
     async def webpanel(self, ctx):
-        await ctx.send("<https://bigcityrp.com/panel/login.php>")
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Webpanel", value="https://bigcityrp.com/panel/login.php", inline=False)
+        await ctx.send(embed=embed)
         await ctx.message.delete()
 
     @commands.command(pass_context = True)
     async def rules(self,ctx):
-        await ctx.send("<https://bigcityrp.com/forums/index.php?/topic/4005-server-rules/>")
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Server Rules", value="https://bigcityrp.com/forums/index.php?/topic/4005-server-rules/", inline=False)
+        await ctx.send(embed=embed)
         await ctx.message.delete()
 
     @commands.command(pass_context = True)
@@ -45,12 +51,32 @@ class links:
 
     @commands.command(pass_context = True)
     async def donate(self, ctx):
-        await ctx.send("<https://bigcityrp.com/forums/index.php?/donate/>")
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Donations", value="https://bigcityrp.com/forums/index.php?/donate/", inline=False)
+        await ctx.send(embed=embed)
         await ctx.message.delete()
 
     @commands.command(pass_context = True)
     async def support(self, ctx):
-        await ctx.send("<https://bigcityrp.com/forums/index.php?/support/>")
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Support", value="https://bigcityrp.com/forums/index.php?/support/", inline=False)
+        await ctx.send(embed=embed)
+        await ctx.message.delete()
+
+    @commands.command(pass_context = True)
+    async def powergaming(self, ctx):
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Powergaming", value="Exploiting routes of roleplay or game mechanics to gain an unfair advantage over someone else.", inline=False)
+        embed.set_footer(text="For more rules and definitions please visit our forums")
+        await ctx.send(embed=embed)
+        await ctx.message.delete()
+
+    @commands.command(pass_context = True)
+    async def copbaiting(self, ctx):
+        embed=discord.Embed(color=0x0080ff)
+        embed.add_field(name="Copbaiting", value="Forcing RP with members of the police department through unrealistic actions no real human would do.", inline=False)
+        embed.set_footer(text="For more rules and definitions please visit our forums")
+        await ctx.send(embed=embed)
         await ctx.message.delete()
 
 def setup(bot):
